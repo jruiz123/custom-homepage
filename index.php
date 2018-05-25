23e183e9235dc0eff6d3ea8d7cf46fe29a1ef386
jruiz123 <acajegas@ASI.ALLIANCE.COM.PH>
2018-05-25 18:39:19 +0800

    Update for custom sidebar

--- index.php
+++ index.php
@@ -15,8 +15,20 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
-		<div id="sidebar" class="sbar-h bg-4">
-			Sample
+		<div id="sidebar" class="sbar-h">
+			<div id="sidebar-header" class="row px-2 py-2 bg-1">
+				<div class="col-8">
+					<p id="sidebar-title" class="h2 text-white"></p>
+				</div>
+				<div class="col-4 text-right">
+					<button class="btn text-white sq-border">
+						<i class="fa fa-times fa-1x"></i>
+					</button>
+				</div>
+			</div>
+			<div>
+				Sample
+			</div>
 		</div>
 		<div class="container">
 			<div class="row my-2">
@@ -38,77 +50,77 @@
 			<!-- Row 1 -->
 			<div class="row my-2 align-items-center">
 				<div class="col-md-3 col-sm-6">
-					<a id="todo" href="" class="card bg-1">
+					<div id="todo" href="" class="card bg-1">
 						<div class="card-body mx-auto">
 							<i class="fas fa-clipboard-list fa-5x"></i>
 						</div>
-						<p class="text-center">To Do List</p>
-					</a>
+						<p class="text-center" id="todo-caption">To Do List</p>
+					</div>
 				</div>
 				<div class="my-2 col-sm-12 d-block d-sm-none"></div>
 				<div class="col-md-3 col-sm-6">
-					<a id="notes" href="" class="card bg-2">
+					<div id="notes" href="" class="card bg-2">
 						<div class="card-body mx-auto">
 							<i class="fas fa-sticky-note fa-5x"></i>
 						</div>
-						<p class="text-center">Notes</p>
-					</a>
+						<p class="text-center" id="notes-caption">Notes</p>
+					</div>
 				</div>
 				<div class="my-2 col-sm-12 d-block d-sm-none d-none d-sm-block d-md-none"></div>
 				<div class="col-md-3 col-sm-6">
-					<a id="favorite" href="" class="card bg-3">
+					<div id="favorite" href="" class="card bg-3">
 						<div class="card-body mx-auto">
 							<i class="fas fa-star fa-5x"></i>
 						</div>
-						<p class="text-center">Favorite</p>
-					</a>
+						<p class="text-center" id="favorite-caption">Favorite</p>
+					</div>
 				</div>
 				<div class="my-2 col-sm-12 d-block d-sm-none"></div>
 				<div class="col-md-3 col-sm-6">
-					<a id="settings" href="" class="card bg-4">
+					<div id="settings" href="" class="card bg-4">
 						<div class="card-body mx-auto">
 							<i class="fas fa-cog fa-5x"></i>
 						</div>
-						<p class="text-center">Settings</p>
-					</a>
+						<p class="text-center" id="settings-caption">Settings</p>
+					</div>
 				</div>
 			</div>
 			<!-- Row 2 -->
 			<div class="row my-4 align-items-center">
 				<div class="col-md-3 col-sm-6">
-					<a href="" class="card bg-5">
+					<div id="gmail" class="card bg-5">
 						<div class="card-body mx-auto">
 							<i class="fas fa-envelope fa-5x"></i>
 						</div>
-						<p class="text-center">Gmail</p>
-					</a>
+						<p class="text-center" id="gmail-caption">Gmail</p>
+					</div>
 				</div>
 				<div class="my-2 col-sm-12 d-block d-sm-none"></div>
 				<div class="col-md-3 col-sm-6">
-					<a href="" class="card bg-6">
+					<div id="sbi" class="card bg-6">
 						<div class="card-body mx-auto">
 							<i class="fas fa-briefcase fa-5x"></i>
 						</div>
-						<p class="text-center">SBI</p>
-					</a>
+						<p class="text-center" id="sbi-caption">SBI</p>
+					</div>
 				</div>
 				<div class="my-2 col-sm-12 d-block d-sm-none d-none d-sm-block d-md-none"></div>
 				<div class="col-md-3 col-sm-6">
-					<a href="" class="card bg-7">
+					<div id="learn" class="card bg-7">
 						<div class="card-body mx-auto">
 							<i class="fas fa-lightbulb fa-5x"></i>
 						</div>
-						<p class="text-center">Learn</p>
-					</a>
+						<p class="text-center" id="learn-caption">Learn</p>
+					</div>
 				</div>
 				<div class="my-2 col-sm-12 d-block d-sm-none"></div>
 				<div class="col-md-3 col-sm-6">
-					<a href="" class="card bg-8">
+					<div id="social" class="card bg-8">
 						<div class="card-body mx-auto">
 							<i class="fas fa-share-alt-square fa-5x"></i>
 						</div>
-						<p class="text-center">Social</p>
-					</a>
+						<p class="text-center" id="social-caption">Social</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -119,13 +131,23 @@
 		<script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
 		<!-- Custom Script -->
 		<script type="text/javascript">
-		$(document).ready(function() {
-			$('a.card').click(function(){
-				window.alert($(this).attr('id'));
-				$('#sidebar').removeClass('sbar-h');
-				$('#sidebar').addClass('sbar-o');
+			$(document).ready(function() {
+				$('.card').click(function(){
+					$('#sidebar').toggleClass('sbar-h');
+					var id = $(this).attr('id');
+					document.getElementById('sidebar-title').innerHTML = document.getElementById(id+'-caption').innerHTML;
+					if (id == 'todo') {
+						$('#sidebar-header').addClass('bg-1');
+					} else if (id == 'notes') {
+						$('#sidebar-header').addClass('bg-2');
+					} else if (id == 'favorite') {
+						$('#sidebar-header').addClass('bg-3');
+					} else if (id == 'settings') {
+						$('#sidebar-header').addClass('bg-4');
+					}
+
+				});
 			});
-		});
 		</script>
 	</body>
 </html>
