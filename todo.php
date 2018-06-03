ea0a8a8e111c925a393026c753417ff089bc207e
jruiz123 <artjohncajegas@gmail.com>
2018-06-03 22:02:12 +0800

    Added New Files For Offline mode

--- todo.php
+++ todo.php
@@ -1,19 +1,53 @@
-<div>
+<div class="todo-main">
 	<div class="tab-row">
 		<div class="active">Todo</div>
 		<div>Done</div>
 	</div>
 	<div class="px-2 py-2">
-		<div>
+		<div id="todo-tab">
 			<input type="type" class="form-control metro-input" id="search" placeholder="Enter To Add">
-			<hr>
-			<div class="form-check">
+			<hr class="border-line">
+			<ul class="todo-list">
+				<li>
+					<input class="metro-checkbox" type="checkbox" value="" id="checkbox1">
+					<label for="checkbox1">
+						Sample 1
+					</label>
+				</li>
+				<li>
+					<input type="checkbox" value="" id="checkbox2">
+					<label for="checkbox2">
+						Sample 2
+					</label>
+				</li>
+				<li>
+					<input type="checkbox" value="" id="checkbox3">
+					<label for="checkbox3">
+						Sample 3
+					</label>
+				</li>
+			</ul>
+			<!-- <div class="form-check">
 				<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
 				<label class="form-check-label" for="defaultCheck1">
 					Finish code
 				</label>
 			</div>
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+				<label class="form-check-label" for="defaultCheck1">
+					Sample Content 1
+				</label>
+			</div>
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+				<label class="form-check-label" for="defaultCheck1">
+					Sample Content 2
+				</label>
+			</div> -->
 		</div>
-		<div>...</div>
+		<div id="done-tab">
+			...
+		</div>
 	</div>
 </div>
